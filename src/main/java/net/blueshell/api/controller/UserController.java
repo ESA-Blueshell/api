21b11621d91d5df72eda98c97c5e640d8704ef98
swordiemen <tim.blokk@gmail.com>
2019-03-20 20:12:33 +0100

    Added UserController.

--- src/main/java/net/blueshell/api/controller/UserController.java
+++ src/main/java/net/blueshell/api/controller/UserController.java
@@ -1,9 +1,51 @@
 package net.blueshell.api.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import net.blueshell.api.constants.StatusCodes;
+import net.blueshell.api.daos.Dao;
+import net.blueshell.api.daos.UserDao;
+import net.blueshell.api.model.User;
+import org.springframework.web.bind.annotation.*;
 
-@RestController(value = "/api/user")
+import java.util.List;
+
+@RestController
 public class UserController {
 
+    private final Dao<User> dao = new UserDao();
+
+    @GetMapping(value = "/users")
+    public List<User> getPictures() {
+        return dao.list();
+    }
+
+    @PostMapping(value = "/users")
+    public Object createUser(User user) {
+        try {
+            dao.create(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return StatusCodes.BAD_REQUEST;
+        }
+        return StatusCodes.CREATED;
+    }
+
+    @GetMapping(value = "/users/{id}")
+    public Object getUserById(@PathVariable("id") String id) {
+        User user = dao.getById(Long.parseLong(id));
+        if (user == null) {
+            return StatusCodes.NOT_FOUND;
+        }
+        return user;
+    }
+
+    @DeleteMapping(value = "/users/{id}")
+    public Object deleteUserById(@PathVariable("id") String id) {
+        User pic = dao.getById(Long.parseLong(id));
+        if (pic == null) {
+            return StatusCodes.NOT_FOUND;
+        }
+        dao.delete(Long.parseLong(id));
+        return StatusCodes.OK;
+    }
 
 }
