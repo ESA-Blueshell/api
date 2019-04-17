a43a37a19ea4f5e47c032521c3226aa3ed48a3cc
swordiemen <tim.blokk@gmail.com>
2019-04-17 18:50:45 +0200

    Added ID-only stuff for Event

--- src/main/java/net/blueshell/api/model/Event.java
+++ src/main/java/net/blueshell/api/model/Event.java
@@ -1,9 +1,12 @@
 package net.blueshell.api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -18,14 +21,17 @@ public class Event {
 
     @OneToOne
     @JoinColumn(name = "creator_id")
+    @JsonIgnore
     private User creator;
 
     @OneToOne
     @JoinColumn(name = "last_editor_id")
+    @JsonIgnore
     private User lastEditor;
 
     @OneToOne
     @JoinColumn(name = "committee_id")
+    @JsonIgnore
     private Committee committee;
 
     private String title;
@@ -42,6 +48,7 @@ public class Event {
 
     @OneToOne
     @JoinColumn(name = "banner_id")
+    @JsonIgnore
     private Picture banner;
 
     @Column(name = "price_member")
@@ -51,8 +58,54 @@ public class Event {
     private double publicPrice;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "event")
+    @JsonIgnore
     private Set<EventFeedback> feedbacks;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "event")
+    @JsonIgnore
     private Set<Billable> billables;
+
+    @JsonProperty("creator")
+    public long getCreatorId() {
+        return getCreator() == null ? 0 : getCreator().getId();
+    }
+
+    @JsonProperty("lastEditor")
+    public long getLastEditorId() {
+        return getLastEditor() == null ? 0 : getLastEditor().getId();
+    }
+
+    @JsonProperty("committee")
+    public long getCommitteeId() {
+        return getCommittee() == null ? 0 : getCommittee().getId();
+    }
+
+    @JsonProperty("banner")
+    public long getBannerId() {
+        return getBanner() == null ? 0 : getBanner().getId();
+    }
+
+    @JsonProperty("feedbacks")
+    public Set<Long> getFeedbackIds() {
+        Set<Long> set = new HashSet<>();
+        if (getFeedbacks() == null) {
+            return set;
+        }
+        for (EventFeedback ef : getFeedbacks()) {
+            set.add(ef.getId());
+        }
+        return set;
+    }
+
+    @JsonProperty("billables")
+    public Set<Long> getBillableIds() {
+        Set<Long> set = new HashSet<>();
+        if (getBillables() == null) {
+            return set;
+        }
+        for (Billable b : getBillables()) {
+            set.add(b.getId());
+        }
+        return set;
+    }
 }
