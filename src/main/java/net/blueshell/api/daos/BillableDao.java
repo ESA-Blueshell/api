6d4193236f71d9f18ef86c2555c624107a3aa4ab
Mauk <maukmul@gmail.com>
2019-04-17 19:20:25 +0200

    more daos

--- src/main/java/net/blueshell/api/daos/BillableDao.java
+++ src/main/java/net/blueshell/api/daos/BillableDao.java
@@ -1,17 +1,9 @@
 package net.blueshell.api.daos;
 
-import net.blueshell.api.db.DatabaseManager;
+import net.blueshell.api.db.SessionWrapper;
 import net.blueshell.api.model.Billable;
-import net.blueshell.api.model.Picture;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import java.util.List;
-
-public class BillableDao extends  implements Dao<Billable> {
+public class BillableDao extends SessionWrapper<Billable> implements Dao<Billable> {
 
     public BillableDao() {
         super(Billable.class);
