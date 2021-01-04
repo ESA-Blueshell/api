40454fc5e0896372ad0069f0d83733e51e0b6a5c
s1934538 <p.j.m.smit@student.utwente.nl>
2021-01-04 10:15:54 +0100

    Revert "SessionWrapper now uses Database manager directly instead of creating its own sessionmanager"
    This reverts commit 4e5d2c7e

--- src/main/java/net/blueshell/api/daos/UserDao.java
+++ src/main/java/net/blueshell/api/daos/UserDao.java
@@ -1,6 +1,5 @@
 package net.blueshell.api.daos;
 
-import net.blueshell.api.db.DatabaseManager;
 import net.blueshell.api.db.SessionWrapper;
 import net.blueshell.api.model.User;
 import org.hibernate.Session;
@@ -17,7 +16,7 @@ public class UserDao extends SessionWrapper<User> implements Dao<User>  {
 
     public User getByUsername(String username) {
         User obj;
-        try (Session session = DatabaseManager.getSession()) {
+        try (Session session = getSessionFactory().openSession()) {
             Transaction t = session.beginTransaction();
             Query query = session.createQuery("from users where username = :name");
             query.setParameter("name", username);
--- src/main/java/net/blueshell/api/db/SessionWrapper.java
+++ src/main/java/net/blueshell/api/db/SessionWrapper.java
@@ -1,6 +1,7 @@
 package net.blueshell.api.db;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import javax.persistence.criteria.CriteriaBuilder;
@@ -10,107 +11,94 @@ import java.util.List;
 
 public class SessionWrapper<T> {
 
+    private SessionFactory sessionFactory;
     private Class<T> clazz;
 
     public SessionWrapper(Class<T> clazz) {
         this.clazz = clazz;
+        sessionFactory = DatabaseManager.getSessionFactory();
     }
 
     public List<T> list() {
         List<T> list = null;
-//        boolean done = false;
-//        while (!done) {
-        try (Session session = DatabaseManager.getSession()) {
-            session.close();
-            Transaction t = session.beginTransaction();
-            CriteriaBuilder builder = session.getCriteriaBuilder();
-            CriteriaQuery<T> criteriaQuery = builder.createQuery(clazz);
-            Root<T> tRoot = criteriaQuery.from(clazz);
-            criteriaQuery.select(tRoot);
-            list = session.createQuery(criteriaQuery).getResultList();
-            t.commit();
-
-//            } catch (Exception e) {
-//                System.out.println("Well that didn't work, let's try again");
-//            }
-        } catch (Exception e) {
-            System.out.println("OK WHAT THE ACTUAL FUCKKKK");
-            e.printStackTrace();
+        boolean done = false;
+        while (!done) {
+            try (Session session = sessionFactory.openSession()) {
+                Transaction t = session.beginTransaction();
+                CriteriaBuilder builder = session.getCriteriaBuilder();
+                CriteriaQuery<T> criteriaQuery = builder.createQuery(clazz);
+                Root<T> tRoot = criteriaQuery.from(clazz);
+                criteriaQuery.select(tRoot);
+                list = session.createQuery(criteriaQuery).getResultList();
+                t.commit();
+                done = true;
+            } catch (Exception e) {
+                System.out.println("Well that didn't work, let's try again");
+            }
         }
         return list;
     }
 
     public T getById(long id) {
         T obj = null;
-//        boolean done = false;
-//        while (!done) {
-        try (Session session = DatabaseManager.getSession()) {
-            Transaction t = session.beginTransaction();
-            obj = session.find(clazz, id);
-            t.commit();
-
-//            } catch (Exception e) {
-//                System.out.println("Well that didn't work, let's try again");
-//            }
-        } catch (Exception e) {
-            System.out.println("OK WHAT THE ACTUAL FUCKKKK");
-            e.printStackTrace();
+        boolean done = false;
+        while (!done) {
+            try (Session session = sessionFactory.openSession()) {
+                Transaction t = session.beginTransaction();
+                obj = session.find(clazz, id);
+                t.commit();
+                done = true;
+            } catch (Exception e) {
+                System.out.println("Well that didn't work, let's try again");
+            }
         }
         return obj;
     }
 
     public T create(T object) {
-//        boolean done = false;
-//        while (!done) {
-        try (Session session = DatabaseManager.getSession()) {
-            Transaction t = session.beginTransaction();
-            session.save(object);
-            t.commit();
-
-//            } catch (Exception e) {
-//                System.out.println("Well that didn't work, let's try again");
-//            }
-        } catch (Exception e) {
-            System.out.println("OK WHAT THE ACTUAL FUCKKKK");
-            e.printStackTrace();
-
+        boolean done = false;
+        while (!done) {
+            try (Session session = sessionFactory.openSession()) {
+                Transaction t = session.beginTransaction();
+                session.save(object);
+                t.commit();
+                done = true;
+            } catch (Exception e) {
+                System.out.println("Well that didn't work, let's try again");
+            }
         }
         return object;
     }
 
     public void update(T object) {
-//        boolean done = false;
-//        while (!done) {
-        try (Session session = DatabaseManager.getSession()) {
-            Transaction t = session.beginTransaction();
-            session.update(object);
-            t.commit();
-
-//            } catch (Exception e) {
-//                System.out.println("Well that didn't work, let's try again");
-//            }
-        } catch (Exception e) {
-            System.out.println("OK WHAT THE ACTUAL FUCKKKK");
-            e.printStackTrace();
+        boolean done = false;
+        while (!done) {
+            try (Session session = sessionFactory.openSession()) {
+                Transaction t = session.beginTransaction();
+                session.update(object);
+                t.commit();
+                done = true;
+            } catch (Exception e) {
+                System.out.println("Well that didn't work, let's try again");
+            }
         }
-
     }
 
     public void delete(long id) {
-//        boolean done = false;
-//        while (!done) {
-        try (Session session = DatabaseManager.getSession()) {
-            Transaction t = session.beginTransaction();
-            session.remove(getById(id));
-            t.commit();
-
-//            } catch (Exception e) {
-//                System.out.println("Well that didn't work, let's try again");
-//            }
-        } catch (Exception e) {
-            System.out.println("OK WHAT THE ACTUAL FUCKKKK");
-            e.printStackTrace();
+        boolean done = false;
+        while (!done) {
+            try (Session session = sessionFactory.openSession()) {
+                Transaction t = session.beginTransaction();
+                session.remove(getById(id));
+                t.commit();
+                done = true;
+            } catch (Exception e) {
+                System.out.println("Well that didn't work, let's try again");
+            }
         }
     }
 
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 }
