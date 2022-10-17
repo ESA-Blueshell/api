07e681d8e03934f76207ed3b17e7dfa9d423f8fd
Peter Smit <p.j.m.smit@student.utwente.nl>
2022-10-17 15:51:57 +0200

    SERVICE ACCOUNTS BAYBEEEE LETS GOOO

--- src/main/java/net/blueshell/api/business/event/EventController.java
+++ src/main/java/net/blueshell/api/business/event/EventController.java
@@ -1,17 +1,14 @@
 package net.blueshell.api.business.event;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.wordnik.swagger.annotations.ApiParam;
 import net.blueshell.api.business.user.Role;
 import net.blueshell.api.business.user.User;
@@ -41,32 +38,24 @@ public class EventController extends AuthorizationController {
     //TODO: CHANGE TO BLUESHELL CALENDAR!!!!!!!!!1!!!!!111!!
     //TODO: This is now the sitecie calendar lol
     private static final String GOOGLE_CALENDAR_ID = "9cugc57n2rc43p6o1r0povepe0@group.calendar.google.com";
+    private static final String APPLICATION_NAME = "Blueshell Google Calendar API";
+    private static final String CREDENTIALS_FILENAME = "credentials.json";
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private static final List<String> SCOPES = List.of(CalendarScopes.CALENDAR_EVENTS);
 
     private final Dao<Event> dao = new EventDao();
     private final StorageService storageService;
     private Calendar service;
 
     {
-        final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
-        final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-        final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_EVENTS);
-        final String TOKENS_DIRECTORY_PATH = "tokens";
         try {
             final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            InputStream in = new FileInputStream("credentials.json");
-            GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-            // Build flow and trigger user authorization request.
-            GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                    HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                    .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                    .setAccessType("offline")
-                    .build();
-            LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-            Credential credentials = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+            GoogleCredentials credentials = ServiceAccountCredentials
+                    .fromStream(new FileInputStream(CREDENTIALS_FILENAME))
+                    .createScoped(SCOPES);
 
             // Build a new authorized API client service.
-            service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credentials)
+            service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpCredentialsAdapter(credentials))
                     .setApplicationName(APPLICATION_NAME)
                     .build();
         } catch (IOException | GeneralSecurityException e) {
