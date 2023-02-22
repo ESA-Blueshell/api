84ec8a230f9a17aee4e10768a2f43a0d7a486491
Peter Smit <p.j.m.smit@student.utwente.nl>
2023-02-22 18:08:27 +0100

    past events now limited to 30

--- src/main/java/net/blueshell/api/business/event/EventController.java
+++ src/main/java/net/blueshell/api/business/event/EventController.java
@@ -163,7 +163,7 @@ public class EventController extends AuthorizationController {
         return dao.list().stream()
                 .filter(predicate)
                 .sorted(Comparator.comparing(Event::getStartTime))
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()).subList(0,30);
     }
 
 
