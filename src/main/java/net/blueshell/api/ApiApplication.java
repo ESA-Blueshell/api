5b987d84706350d8677522d69f4c0dd757cab944
Peter Smit <peter@elnino.tech>
2023-12-23 12:24:48 +0100

    Move constants to application.properties

--- src/main/java/net/blueshell/api/ApiApplication.java
+++ src/main/java/net/blueshell/api/ApiApplication.java
@@ -1,20 +1,15 @@
 package net.blueshell.api;
 
-import net.blueshell.api.db.DatabaseManager;
 import net.blueshell.api.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class ApiApplication {
 
     public static void main(String[] args) {
-        System.out.println("[DB] Init");
-        DatabaseManager.init();
-        System.out.println("[DB] Done");
         System.setProperty("server.servlet.context-path", "/api");
         SpringApplication.run(ApiApplication.class, args);
         initEvents();
--- src/main/java/net/blueshell/api/db/DatabaseManager.java
+++ src/main/java/net/blueshell/api/db/DatabaseManager.java
@@ -12,11 +12,13 @@ import net.blueshell.api.business.user.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 /**
  * Created on 12/12/2017.
@@ -26,32 +28,32 @@ public class DatabaseManager {
     private static SessionFactory sessionFactory;
     private static Configuration config;
 
+    @Value("${spring.datasource.url}")
+    private String url;
 
-    public static void init() {
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    public void init() {
         Configuration configuration = new Configuration()
+                .setProperty("hibernate.connection.url", url)
+                .setProperty("hibernate.connection.username", username)
+                .setProperty("hibernate.connection.password", password)
                 .setProperty("connection.driver_class", "com.mysql.cj.jdbc.Driver")
                 .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
-                .setProperty("hibernate.connection.url", "jdbc:mysql://127.0.0.1:3306/blueshell?autoReconnect=true&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true")
-                .setProperty("hibernate.connection.username", "root")
-                .setProperty("hibernate.connection.password", "password")
                 .setProperty("connection.pool_size", "100")
                 .setProperty("connection.autoReconnect", "true")
                 .setProperty("connection.autoReconnectForPools", "true")
                 .setProperty("connection.is-connection-validation-required", "true")
-                .setProperty("connection.pool_size", "10000")
-
                 .setProperty("hibernate.enable_lazy_load_no_trans", "true")
-
                 .setProperty("hibernate.show_sql", "false")
-
                 .setProperty("hibernate.format_sql", "true")
-
                 .setProperty("hibernate.use_sql_comments", "true");
 
-
-        config = configuration;
-        configuration.setProperty("autoReconnect", "true");
-        Class[] dbClasses = new Class[]{
+        List.of(
                 Committee.class,
                 Event.class,
                 EventSignUp.class,
@@ -60,11 +62,10 @@ public class DatabaseManager {
                 Sponsor.class,
                 News.class,
                 User.class,
-                CommitteeMembership.class,
-        };
-        for (Class clazz : dbClasses) {
-            configuration.addAnnotatedClass(clazz);
-        }
+                CommitteeMembership.class
+        ).forEach(configuration::addAnnotatedClass);
+
+        config = configuration;
         sessionFactory = configuration.buildSessionFactory();
     }
 
@@ -81,7 +82,13 @@ public class DatabaseManager {
     }
 
     @Bean
-    public static DataSource dataSource() {
+    public DataSource dataSource() {
+        if (config == null || sessionFactory == null) {
+            System.out.println("[DB] Init");
+            init();
+            System.out.println("[DB] Done");
+        }
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         org.hibernate.cfg.Configuration config = DatabaseManager.getConfig();
         dataSource.setDriverClassName(config.getProperty("connection.driver_class"));
