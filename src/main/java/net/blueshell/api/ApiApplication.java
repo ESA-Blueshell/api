f4f35b31f3b3a71b50f4921c989e9adc850b6dda
j.jonkers <j.jonkers@dynalynx.nl>
2025-04-15 13:05:01 +0200

    Refactor of communicators and social media service

--- src/main/java/net/blueshell/api/ApiApplication.java
+++ src/main/java/net/blueshell/api/ApiApplication.java
@@ -4,6 +4,7 @@ import net.blueshell.api.config.StorageConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableConfigurationProperties(StorageConfig.class)
 @EnableJpaAuditing
 @EnableAsync
+@ComponentScan(basePackages = {"net.blueshell.api", "net.blueshell.common", "net.blueshell.db"})
 public class ApiApplication {
 
     public static void main(String[] args) {
