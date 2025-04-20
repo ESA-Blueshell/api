f35946a7a3367e598bd899d688b9c9fed3449aa3
Joris Jonkers <joriswouter@live.nl>
2025-04-20 13:36:42 +0200

    Adding nginx

--- src/main/java/net/blueshell/api/ApiApplication.java
+++ src/main/java/net/blueshell/api/ApiApplication.java
@@ -15,7 +15,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @ComponentScan(basePackages = {"net.blueshell.api", "net.blueshell.common", "net.blueshell.db"})
 public class ApiApplication {
     public static void main(String[] args) {
-        System.setProperty("server.servlet.context-path", "/api");
         SpringApplication.run(ApiApplication.class, args);
     }
 }
